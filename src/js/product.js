5ea702ef4578741279f4dd0a8be55741193d5586
MonicaBarreraC <u653058995@byui.edu>
2024-01-13 21:46:41 -0600

    Imported getlocalStorage function, fixed addProductToCart function

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get current products in cart
+  let cart = getLocalStorage("so-cart")
+  // If there are no products in cart, create an empty array
+  if (cart == null){
+    cart = [];
+  }
+  // Add the selected product into the cart
+  cart.push(product);
+  // Rewrite previous cart
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
