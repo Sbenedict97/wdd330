5803db501fdc267350fde33717776b64c01cdef0
MonicaBarreraC <u653058995@byui.edu>
2024-01-19 12:16:12 -0600

    Added product card template - step 5

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,3 +1,14 @@
+function productCardTemplate(product) {
+    return `<li class="product-card">
+                <a href="product_pages/?product=">
+                    <img src="" alt="Image of "/>
+                    <h3 class="card__brand"></h3>
+                    <h2 class="card__name"></h2>
+                    <p class="product-card__price">$</p>
+                </a>
+            </li>`;
+}
+
 export default class ProductListing {
     constructor(category, dataSource, listElement) {
       this.category = category;
@@ -7,5 +18,10 @@ export default class ProductListing {
     
     async init() {
       const list = await this.dataSource.getData();
+    }
+
+    renderList(list) {
+        const htmlStrings = list.map(productCardTemplate);
+        this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
     }
   }
