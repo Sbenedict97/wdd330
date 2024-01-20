a9adc50b3a220327ace7f6087d74b9e9e9204c34
alexander chagua <alexanderelton2887@gmail.com>
2024-01-20 18:42:54 -0500

    NEWALEX

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,24 @@
+
+
+export default class ProductListing {
+    constructor(category, dataSource, listElement) {
+      // We passed in this information to make our class as reusable as possible.
+      // Being able to define these things when we use the class will make it very flexible
+      this.category = category;
+      this.dataSource = dataSource;
+      this.listElement = listElement;
+    }
+
+
+    
+    
+
+    async init() {
+      // our dataSource will return a Promise...so we can use await to resolve it.
+      const list = await this.dataSource.getData();
+      // render the list - to be completed
+    }
+  }
+
+ 
+
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+
+import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
+
+// Crea una instancia del módulo
+const productInstance = new ProductData();
+const productListings = new ProductListing();
