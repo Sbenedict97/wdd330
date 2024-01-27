6b65b5aef4473e2fc2d3cd634255b8d433e80691
MonicaBarreraC <u653058995@byui.edu>
2024-01-27 15:42:25 -0600

    Duplicate Items in Cart

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -39,7 +39,7 @@ function cartItemTemplate(item) {
       <h2 class="card__name">${item.Name}</h2>
     </a>
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__quantity">qty: ${item.Quantity}</p>
     <p class="cart-card__price">$${item.FinalPrice}</p>
   </li>`;
   
