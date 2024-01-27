e6db95aad7663ec77208fee1d424ea18d5d1157a
yhipolito <yazelhipolito@gmail.com>
2024-01-27 07:50:01 -0700

    filled product-listing.js

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,5 +1,8 @@
+import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
 const listing = new ProductListing("Tents", dataSource, element);
 
 listing.init();
