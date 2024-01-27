1c1418891359ee9a5f80a114c629b2a5ddfa82df
janZ <janzander96@gmail.com>
2024-01-27 14:42:17 +0800

    no products are being rendered when accessing the category pages

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -10,7 +10,8 @@ function convertToJson(res) {
 
 export default class ProductData {
   constructor(category) {
-
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
   }
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
@@ -18,7 +19,8 @@ export default class ProductData {
     return data.Result;
   }
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    const response = await fetch(baseURL + `product/${id}`);
+    const data = await convertToJson(response);
+    return data.Result;
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
+import { loadHeaderFooter } from "./utils.mjs";
+/* 
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const listing = new ProductListing("Tents", dataSource, element);
 
-listing.init();
+listing.init(); 
+*/
 
 loadHeaderFooter();
