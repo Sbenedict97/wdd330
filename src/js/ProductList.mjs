32ee4d6ef28589f9a530dd7729d46508f53728ee
yhipolito <yazelhipolito@gmail.com>
2024-01-27 10:53:13 -0700

    change made to productList.mjs

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,7 +2,7 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
   return `<li class="product-card">
-  <a href="product_pages/index.html?product=${product.Id}">
+  <a href="/product_pages/index.html?product=${product.Id}">
   <img
     src="${product.Image}"
     alt="Image of ${product.Name}"
