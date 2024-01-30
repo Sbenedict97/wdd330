a1eb96185a58844bd6760fb15c065733aead0f42
yhipolito <yazelhipolito@gmail.com>
2024-01-30 06:23:45 -0700

    updated ProductData.mjs.getData and findProductById

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -13,12 +13,12 @@ export default class ProductData {
     
   }
   async getData(category) {
-    const response = await fetch(baseURL + `/products/search/${category}`);
+    const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
   }
   async findProductById(id) {
-    const response = await fetch(baseURL + `/product/${id}`);
+    const response = await fetch(baseURL + `product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
   }
