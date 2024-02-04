8da4d9d5fc1fccd732b4ab695721eefc55db457b
MonicaBarreraC <u653058995@byui.edu>
2024-02-03 18:44:39 -0600

    External Services last update- not complete

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -8,18 +8,18 @@ function convertToJson(res) {
   }
 }
 
-export default class ProductData {
+export default class ExternalServices {
   constructor(category) {
     // this.category = category;
     // this.path = `../json/${this.category}.json`;
   }
   async getData(category) {
-    const response = await fetch(baseURL + `/products/search/${category}`);
+    const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
   }
   async findProductById(id) {
-    const response = await fetch(baseURL + `/product/${id}`);
+    const response = await fetch(baseURL + `product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
   }
