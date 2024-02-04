7c0816b7635ee3844b63fba858d86d4737e09217
MonicaBarreraC <u653058995@byui.edu>
2024-02-03 22:26:29 -0600

    Lint and Format

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -10,11 +10,10 @@ document
   .querySelector("#zip")
   .addEventListener("blur", myCheckout.calculateOrdertotal.bind(myCheckout));
 // listening for click on the button
-document.querySelector('#checkoutSubmit').addEventListener('click', (e) => {
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
   e.preventDefault();
   const myForm = document.forms[0];
   const chk_status = myForm.checkValidity();
   myForm.reportValidity();
-  if(chk_status)
-    myCheckout.checkout();
-  });
+  if (chk_status) myCheckout.checkout();
+});
